0ba924100e2b1488f595a21fbb26e1155036098d
Shaikh Aman <110104595+ShaikhAman01@users.noreply.github.com>
2024-07-26 18:39:49 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,9 @@
 const express = require("express");
+var cors = require('cors');
+const mainRouter = require("./routes/index");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-
+app.use("/api/v1", mainRouter);
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const userRouter = require("./user")
+
+const router = express.Router();
+
+app.use("/user",userRouter);
+
+module.exports = router;
+
